6144103cab1bc5a6d30c90fabb24d0b85ccb4479
blueslaboratory <myjunkandspam22@gmail.com>
2022-12-08 13:59:15 +0100

    day 33

--- 019 Socket TCP Server/src/entregable1/ParesNonesTCPServer.java
+++ 019 Socket TCP Server/src/entregable1/ParesNonesTCPServer.java
@@ -1,3 +1,4 @@
+// TCP - Transmission Control Protocol
 /*
 
 Ejercicio 2 - ¿PARES o NONES?
@@ -15,42 +16,206 @@ antes.
 
 EJEMPLO DE SALIDA:
 
-El juega: PARES
+El jugador juega: PARES
 Nº: 34
 Se ha generado un numero aleatorio: 25
+Resultado: 59 - NONES
 He ganado.
 Servidor: 1 Cliente: 0
 
-El juega: NONES
+El jugador juega: NONES
 Nº: 23
 Se ha generado un numero aleatorio: 75
+Resultado: 98 - PARES
 He ganado.
 Servidor: 2 Cliente: 0
 
-El juega: PARES
+El jugador juega: PARES
 Nº: 56
 Se ha generado un numero aleatorio: 34
+Resultado: 90 - PARES
 He perdido.
 Servidor: 2 Cliente: 1
 
-El juega: PARES
+El jugador juega: PARES
 Nº: 2
 Se ha generado un numero aleatorio: 54
+Resultado: 56 - PARES
 He perdido.
 Servidor: 2 Cliente: 2
 
-El juega: NONES
+El jugador juega: NONES
 Nº: 7
 Se ha generado un numero aleatorio: 14
+Resultado: 21 - NONES
 He ganado.
 Servidor: 2 Cliente: 3
 */
 
 package entregable1;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ParesNonesTCPServer {
 
+	public static int SCORECLIENTE = 0;
+	public static int SCORESERVIDOR = 0;
+	
 	public static void main(String[] args) {
+		// ASIGNO UN PUERTO A MI APLICACION E INSTANCIO UN SERVERSOCKET CON EL PUERTO
+		// ASI COMO UN SOCKET PARA CUANDO SE CONECTA UN CLIENTE
 		
+		try {
+			
+			// Puerto
+			int numeroPuerto = 6069;
+			
+			String paresNones;
+			int numeroPartidas;
+			int counter = 1;
+			int numeroCliente;
+			int numeroServidor;
+			boolean ganaServidor;
+			
+			ServerSocket servidor = new ServerSocket(numeroPuerto);
+			Socket clienteConectado = null;
+			
+	
+			System.out.println("PROGRAMA SERVIDOR INICIADO...");
+			System.out.println("Hola soy el servidor y me tengo que iniciar antes que el cliente");
+			System.out.println("Esperando al cliente...");
+			System.out.println();
+			clienteConectado = servidor.accept();
+			
+			
+			// CREO FLUJO DE ENTRADA DEL CLIENTE
+			InputStream entrada = null;
+			entrada = clienteConectado.getInputStream();
+			DataInputStream flujoEntrada = new DataInputStream(entrada);
+			
+			// CREO FLUJO DE SALIDA AL CLIENTE
+			OutputStream salida = null;
+			salida = clienteConectado.getOutputStream();
+			DataOutputStream flujoSalida = new DataOutputStream(salida);
+			
+			
+			
+			numeroPartidas = flujoEntrada.readInt();
+			System.out.println("Numero de partidas a jugar: " +numeroPartidas);
+			
+			while(numeroPartidas>0) {
+				
+				System.out.println();
+				System.out.println("*** PARTIDA " +counter +" ***");
+				
+				
+				// Programa servidor: El servidor generara un aleatorio y se lo enviara al cliente.
+				// El cliente me envia pares o nones y el numero que juega
+				paresNones = flujoEntrada.readUTF();
+				System.out.println("El jugador juega " +paresNones.toUpperCase());
+				numeroCliente = Integer.parseInt(flujoEntrada.readUTF());
+				System.out.println("nº: " +numeroCliente);
+				numeroServidor = aleatorio();
+				System.out.println("Se ha generado un numero aleatorio: " +numeroServidor); 
+				
+				// Envio el mensaje al cliente
+				flujoSalida.writeUTF(String.valueOf(numeroServidor));
+				
+				
+				// calculo resultadoJuego
+				ganaServidor = !resultadoJuego(paresNones, numeroCliente, numeroServidor);
+				
+				if(ganaServidor) {
+					System.out.println("He ganado");
+				}
+				else {
+					System.out.println("He perdido");
+				}
+				
+				System.out.println("Servidor: " +SCORESERVIDOR +" Cliente: " +SCORECLIENTE);
+				System.out.println();
+				
+				numeroPartidas--;
+				counter++;
+			}
+			
+			
+			
+			// CERRAR STREAMS Y SOCKETS	
+			System.out.println();
+			System.out.println();
+			System.out.println("Fin del juego");
+			
+			entrada.close();
+			flujoEntrada.close();
+			
+			salida.close();
+			flujoSalida.close();
+			
+			clienteConectado.close();
+			servidor.close();
+		
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		
+		
+	}
+	
+	
+	public static int aleatorio() {
+		// En general, para conseguir un numero entero entre [N, M]
+		// con N < M y ambos incluidos, debemos usar esta formula:
+		// int valorEntero = Math.floor(Math.random()*(M-N+1)+N);
+		// Valor entre M y N, ambos incluidos.
+
+		int aleatorio = (int) Math.floor(Math.random()*(100-0+1)+0);
+		
+		return aleatorio;
+	}
+	
+	
+	public static boolean resultadoJuego(String paresNones, int nClient, int nServer) {
+		boolean ganaCliente = true;
+		String resultado = "NONES";
+		
+		int nResultado = nClient + nServer;
+		
+		
+		if(paresNones.equalsIgnoreCase("NONES")) {
+			if(nResultado%2==0) {
+				ganaCliente = false;
+				resultado = "PARES";
+				SCORESERVIDOR++;
+			}
+			else {
+				SCORECLIENTE++;
+			}
+		}
+		if(paresNones.equalsIgnoreCase("PARES")) {
+			if(nResultado%2==0) {
+				resultado = "PARES";
+				SCORECLIENTE++;
+			}
+			else {
+				ganaCliente = false;
+				SCORESERVIDOR++;
+			}
+		}
+		
+		
+		System.out.println("Resultado: " +nResultado +" - " +resultado);
+		
+		
+		return ganaCliente;
 	}
 }
--- 020 Socket TCP Client/src/entregable1/ParesNonesTCPClient.java
+++ 020 Socket TCP Client/src/entregable1/ParesNonesTCPClient.java
@@ -1,3 +1,4 @@
+// TCP - Transmission Control Protocol
 /*
 
 Ejercicio 2 - ¿PARES o NONES?
@@ -15,28 +16,38 @@ antes.
 
 EJEMPLO DE SALIDA:
 
-Dime un numero:
+PARES O NONES: PARES
+Dime un numero: 34
 El juega: 25
+Resultado: 59 - NONES
 He perdido.
 Cliente: 0 Servidor: 1 
 
+PARES O NONES: NONES
 Dime un numero: 23
 El juega: 75
+Resultado: 98 - PARES
 He perdido.
 Cliente: 0 Servidor: 2
 
+PARES O NONES: PARES
 Dime un numero: 56
 El juega: 34
+Resultado: 90 - PARES
 He ganado.
 Cliente: 1 Servidor: 2
 
+PARES O NONES: PARES
 Dime un numero: 2
 El juega: 54
+Resultado: 56 - PARES
 He ganado.
 Cliente: 2 Servidor: 2
 
+PARES O NONES: NONES
 Dime un numero: 7
 El juega: 14
+Resultado: 21 - NONES
 He ganado.
 Cliente: 3 Servidor: 2
 */
@@ -44,11 +55,162 @@ Cliente: 3 Servidor: 2
 
 package entregable1;
 
-public class ParesNonesTCPClient {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class ParesNonesTCPClient {
+	
+	public static int SCORECLIENTE = 0;
+	public static int SCORESERVIDOR = 0;
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try {
+			
+			// Host -> "localhost";
+			//String Host = "192.168.56.1";
+			String host = "localhost";
+			
+			// puerto remoto
+			int numeroPuerto = 6069;
+			
+			Scanner sc = new Scanner(System.in);
+			String paresNones;
+			int numeroPartidas;
+			int counter = 1;
+			String numeroCliente;
+			String numeroServidor;
+			boolean ganaCliente;
+			
+			
+			System.out.println("PROGRAMA CLIENTE INICIADO...");
+			System.out.println("Hola soy el cliente y me inicio despues del servidor");
+			System.out.println();
+			Socket Cliente = new Socket(host, numeroPuerto);
+			
+			
+			// CREO FLUJO DE SALIDA AL SERVIDOR
+			DataOutputStream flujoSalida = new DataOutputStream(Cliente.getOutputStream());
+			
+			// CREO FLUJO DE ENTRADA DEL SERVIDOR
+			DataInputStream flujoEntrada = new DataInputStream(Cliente.getInputStream());
+			
+			
+			
+			
+			System.out.print("Numero de partidas a jugar: ");
+			numeroPartidas = Integer.parseInt(sc.nextLine());
+			flujoSalida.writeInt(numeroPartidas);
+			
+			// Programa cliente:
+			// Una de las aplicaciones le pedira al usuario por pantalla el juego a jugar 
+			// y el numero que quiere jugar. Se lo enviara a la otra que actua de servidor.
+						
+			
+			while(numeroPartidas>0) {
+				
+				
+				System.out.println();
+				System.out.println("*** PARTIDA " +counter +" ***");
+				
+				do {
+					System.out.print("PARES O NONES: ");
+					paresNones = sc.nextLine();
+				} while(!(paresNones.equalsIgnoreCase("PARES") || 
+						  paresNones.equalsIgnoreCase("NONES")));
+				
+				System.out.print("nº: ");
+				numeroCliente = sc.nextLine();
+
+				// Programa cliente: envia PARES o NONES
+				flujoSalida.writeUTF(paresNones);
+				// Programa cliente: envia el numero
+				flujoSalida.writeUTF(numeroCliente);
+				
+				// El servidor me envia el numero que juega
+				System.out.print("El juega: ");
+				numeroServidor = flujoEntrada.readUTF();
+				System.out.println(numeroServidor);		
+				
+				
+				// calculo resultadoJuego
+				ganaCliente = resultadoJuego(paresNones, numeroCliente, numeroServidor);
+				
+				if(ganaCliente) {
+					System.out.println("He ganado");
+				}
+				else {
+					System.out.println("He perdido");
+				}
+				
+				System.out.println("Cliente: " +SCORECLIENTE +" Servidor: " +SCORESERVIDOR);
+				System.out.println();
+				
+				numeroPartidas--;
+				counter++;
+			} 
+			
+			
+			
+			
+			// CERRAR STREAMS Y SOCKETS	
+			System.out.println();
+			System.out.println();
+			System.out.println("Fin del juego");
+			
+			flujoEntrada.close();
+			flujoSalida.close();
+			Cliente.close();
+			sc.close();
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
-
+	
+	
+	
+	public static boolean resultadoJuego(String paresNones, String numClient, String numServer) {
+		boolean ganaCliente = true;
+		String resultado = "NONES";
+		
+		int nClient = Integer.parseInt(numClient);
+		int nServer = Integer.parseInt(numServer);
+		int nResultado = nClient + nServer;
+		
+		
+		if(paresNones.equalsIgnoreCase("NONES")) {
+			if(nResultado%2==0) {
+				ganaCliente = false;
+				resultado = "PARES";
+				SCORESERVIDOR++;
+			}
+			else {
+				SCORECLIENTE++;
+			}
+		}
+		if(paresNones.equalsIgnoreCase("PARES")) {
+			if(nResultado%2==0) {
+				resultado = "PARES";
+				SCORECLIENTE++;
+			}
+			else {
+				ganaCliente = false;
+				SCORESERVIDOR++;
+			}
+		}
+		
+		
+		System.out.println("Resultado: " +nResultado +" - " +resultado);
+		
+		return ganaCliente;
+	}
+	
 }
